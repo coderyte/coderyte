03ff62fc9a2f94b34d3b254231275061a3ffbb58
Ernest Addae <earnestaddae@gmail.com>
2014-07-25 04:13:31 +0000

    Commence coderyte app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coderyte::Application.config.secret_key_base = 'e066696887a1597030b7b85a2ad9c66be9977903ea9a32ac4abe8461cd77bca6af0ad06bf77d3c139f08bad5ead781604d632a5966b6f620e2f1ae06ae6de3b6'
